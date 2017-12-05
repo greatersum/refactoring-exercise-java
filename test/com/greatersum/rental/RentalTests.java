4878cc42d4a3f8e441e3b6177d43944065247bc7
mdclement <mike@softwareontheside.com>
2017-12-05 01:11:19 -0500

    changed test to use approvals

--- test/com/greatersum/rental/RentalTests.java
+++ test/com/greatersum/rental/RentalTests.java
@@ -1,5 +1,6 @@
 package com.greatersum.rental;
 
+import org.approvaltests.Approvals;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,15 +9,9 @@ import java.util.Arrays;
 public class RentalTests {
     @Test
     public void MartinTest() {
-        String properResult =
-                "Rental Record for martin\n" +
-                        "\tRan\t3.5\n" +
-                        "\tTrois Couleurs: Bleu\t2\n" +
-                        "Amount owed is 5.5\n" +
-                        "You earned 2 frequent renter points\n";
         Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1)));
         RentalInfo info = new RentalInfo();
         String actualResult = info.statement(customer);
-        Assert.assertEquals(properResult, actualResult);
+        Approvals.verify(actualResult);
     }
 }
